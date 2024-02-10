c37d727368bc80b614fb90b1e12bd9343e588379
Badsha Hossain <badshab454@gmail.com>
2024-02-10 12:57:06 +0600

    decending sorted string

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-function merge(S1, S2) {
+function ReverseSort(str) {
   //your code here
-  let word = "";
-  let left = 0;
-  let right = 0;
-
-  while (left < S1.length || right < S2.length) {
-    if (left < S1.length) {
-      word += S1[left++];
-    }
-    if (right < S2.length){
-        word += S2[right++]
-    }
-  }
-  return word;
+  let arr = str.split("");
+  let sortedArr = arr.sort();
+  let decendingArr = sortedArr.reverse(); 
+  return decendingArr.join("");
 }
 
-console.log(merge("Hello", "Bye"));
+console.log(ReverseSort("geeks"));
