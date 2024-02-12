63a9eba0025aea545a6876ea0bbab183075b5137
Badsha Hossain <badshab454@gmail.com>
2024-02-12 12:32:07 +0600

    removing space

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-function ReverseSort(str) {
-  //your code here
-  let arr = str.split("");
-  let sortedArr = arr.sort();
-  let decendingArr = sortedArr.reverse(); 
-  return decendingArr.join("");
-}
+let s = "geeks  for geeks"
 
-console.log(ReverseSort("geeks"));
+let result = s.split(" ").join("");
+
+console.log(result);
